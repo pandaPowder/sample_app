a912eea5a33bd0b5fd911cfc3601e93b598ca28d
Dallas Despain <dallas.despain@gmail.com>
2013-04-23 11:35:54 -0600

    finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Dallas Despain"
+    email    "dallas.despain@gmail.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
