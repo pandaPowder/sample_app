34336fa749040b31cdf6adaf818ce11e6b5af621
Dallas Despain <dallas.despain@gmail.com>
2013-04-23 16:49:27 -0600

    added a size attr to gravatar helper changed validation message inderectly by harnessing internationalization library which allowed me to remove the password presence validation switched flash html with content_tag helper... I don't see the point
    added some tests to test validation code on empty signup submission
    added a test to verify that the user was redirected and that user exists

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,6 +29,20 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_selector('.alert-error', text: 'errors') }
+        it { should have_content('error') }
+        it { should have_selector('#error_explanation ul li', text:"Name can't be blank") }
+        it { should have_selector('#error_explanation ul li', text:"Email can't be blank") }
+        it { should have_selector('#error_explanation ul li', text:"Email is invalid") }
+        it { should have_selector('#error_explanation ul li', text:"Password can't be blank") }
+        it { should have_selector('#error_explanation ul li', text:"Password is too short") }
+        it { should have_selector('#error_explanation ul li', text:"Password confirmation can't be blank") }
+        
+      end
     end
 
     describe "with valid information" do
@@ -38,6 +52,15 @@ describe "User pages" do
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
