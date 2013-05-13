1b76a43205ed42ffdef0210164479dbc756bd816
Dallas Despain <dallas.despain@gmail.com>
2013-05-13 15:24:57 -0600

    add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,11 @@ class User < ActiveRecord::Base
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
 
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
+
   validates :name, presence: true, length: { maximum:50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
